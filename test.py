6ae9e6959059ce5a368cc93f05a1d15e6395f6ec
John Bell <jwbell001@gmail.com>
2018-11-07 21:57:31 +0200

    test

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
 import tornado.ioloop
 import tornado.web
 
+HAS_NEO = True
+
+try:
+    import neopixel as neo
+except ImportError:
+    HAS_NEO = False
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
 
+
 if __name__ == "__main__":
+
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
+    print('test')
